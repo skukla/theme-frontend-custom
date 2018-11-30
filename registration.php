a291b8753e550931090be0ffa77ef34933980ee3
Steve Kukla <kukla@adobe.com>
2018-11-29 17:16:43 -0800

    Initial commit

--- registration.php
+++ registration.php
@@ -1,3 +1,3 @@
 <?php
 use \Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Custom/blank', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Custom/rileyrose', __DIR__);
